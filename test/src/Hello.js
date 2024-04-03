d02cbf338095cf75e2a63ebb4ab6ee8219ca3e2e
rlaalsrl1 <yoyo986756@gmail.com>
2024-04-03 12:15:17 +0900

    Create memo

--- test/src/Hello.js
+++
@@ -1,9 +0,0 @@
-class Hello extends React.Component{
-    render(){
-        return <div>Hello{this.props.toWhat}</div>
-    }
-}
-
-// const root = ReactDom.creatRoot(document.getElementById('root'))
-// root.render(<Hello toWhat ="World"/>)
-export default Hello;
