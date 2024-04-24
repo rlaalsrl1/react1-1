7fa342aea69f6446787ec54d638c83599ddb9235
rlaalsrl1 <yoyo986756@gmail.com>
2024-04-24 11:12:20 +0900

    202030405-2

--- midterm/src/App.js
+++ midterm/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Midterm from './Midterm';
 
 function App() {
   
   return (
     <div className="App">
-      <Counter />
+      <Midterm />
     </div>
     
   );
